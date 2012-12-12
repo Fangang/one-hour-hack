82e749c9c3187b02d22604676e12625f40829221
stonepro <stonewf2006@gmail.com>
2012-12-12 19:09:41 +0800

    ld tool usage

---
+++ binutils/ld_tool/hello.s
@@ -0,0 +1,18 @@
+.globl  main
+ 
+.section .text
+main:
+        movl    $len, %edx
+        movl    $msg, %ecx
+        movl    $1, %ebx
+        movl    $4, %eax
+        int     $0x80
+ 
+        movl    $0, %ebx
+        movl    $1, %eax
+        int     $0x80
+.section .data
+msg:
+        .ascii  "Hello, world!\n"
+        len =   . - msg
+
