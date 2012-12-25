0b7bc462e6012921f08d9808e345c15afcc1ce53
susan <eehanwenfang@foxmail.com>
2012-12-25 11:10:25 +0800

    stl and effective tcp/ip

---
+++ STL/cppstl.cpp
@@ -0,0 +1,15 @@
+/* STL study */
+
+#include <iostream>
+#include "cppstl.h"
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+
+	std::cout << "Hello STL" << std::endl;
+	exit(0);
+}
---
+++ STL/cppstl.h
@@ -0,0 +1,19 @@
+namespace cppstl
+{
+	/* be in an object of a class */
+	template <class T>
+	inline const T& max(const T& a, const T& b)
+	{
+		return a<b ? b : a;
+	}
+
+	template <class T>
+	class machine{
+		
+
+	};
+
+
+
+}
+
